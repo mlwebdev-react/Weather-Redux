c6ea06a457b0aca8eeb627f45dae3f077f4527f7
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-25 10:30:24 -0500

    Add SparklinesReferenceLine to chart with line for average temp, pressure, and humity

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Sparklines,  SparklinesLine } from "react-sparklines";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+  }
+  from "react-sparklines";
 
 
 export default (props) => {
@@ -7,6 +12,7 @@ export default (props) => {
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
     </div>
   );
